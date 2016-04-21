dbc98f57191122cfc11c74d11d539e0af92dbb1a
reneedudley <4reneedudley@gmail.com>
2016-04-21 10:19:52 -0500

    built my first component. oh yeah

---
+++ catch-of-the-day/scripts/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM= require('react-dom');
+
+/* Store picker
+This will let us make store picker */
+
+var StorePicker = React.createClass({
+
+	render: function(){
+		return (
+			<p>hi</p> 
+		)
+
+	}
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+
